3ad7503c44a966c249de18cfcb5f097d2731e249
Alex Kagai <alexkagai@outlook.com>
2025-05-20 11:52:38 +0300

    fix Default Units undefined

--- app/lib/config.tsx
+++ app/lib/config.tsx
@@ -1,4 +1,4 @@
-export const APP_ENVIRONMENT = process.env.APP_ENVIRONMENT ?? "PROD";
+export const APP_ENVIRONMENT = process.env.APP_ENVIRONMENT ?? "DEV";
 export const API_ID = process.env.API_ID;
 export const DEFAULT_CITY= process.env.DEFAULT_CITY;
-export const DEFAULT_UNITS = process.env.DEFAULT_UNITS;
+export const DEFAULT_UNITS = process.env.DEFAULT_UNITS ?? "metric";
